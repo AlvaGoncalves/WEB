fd24a4ed5588d3b9b0ca62ba2d6ef5067f9affad
AlvaGoncalves <102428299+AlvaGoncalves@users.noreply.github.com>
2024-02-20 15:00:13 -0300

    Update DragnDrop.js

--- Alvaro/DragnDrop.js
+++ Alvaro/DragnDrop.js
@@ -1,47 +1,26 @@
-// Seleccionamos el elemento que queremos arrastrar
-var caja = document.getElementById("draggable-1");
-
-// Añadimos el evento dragstart para iniciar el arrastre
-caja.addEventListener("dragstart", function(event) {
-  // Guardamos el id del elemento en el objeto dataTransfer
-  event.dataTransfer.setData("Data", caja.id);
-});
-
-// Añadimos el evento drag para mostrar el movimiento
-caja.addEventListener("drag", function(event) {
-  console.log("drag");
-});
-
-// Añadimos el evento dragend para finalizar el arrastre
-caja.addEventListener("dragend", function(event) {
-console.log("dragend");
-});
-
-// Seleccionamos el elemento donde queremos soltar
-var zona = document.querySelector(".example-dropzone");
-
-// Añadimos el evento dragenter para detectar cuando entramos en la zona
-zona.addEventListener("dragenter", function(event) {
-  console.log("dragenter");
-});
-
-// Añadimos el evento dragleave para detectar cuando salimos de la zona
-zona.addEventListener("dragleave", function(event) {
-  console.log("dragleave");
-});
-
-// Añadimos el evento dragover para permitir el arrastre sobre la zona
-zona.addEventListener("dragover", function(event) {
-  console.log("dragover");
-  // Cancelamos el comportamiento por defecto
-  event.preventDefault();
-});
-
-// Añadimos el evento drop para soltar el elemento en la zona
-zona.addEventListener("drop", function(event) {
-  console.log("drop");
-  // Obtenemos el id del elemento arrastrado
-  var caja = event.dataTransfer.getData("Data");
-  // Añadimos el elemento arrastrado a la zona
-  zona.appendChild(document.getElementById(caja));
-});
+// Función para manejar el evento de arrastrar y soltar
+function handleFileSelect(evt) {
+    evt.stopPropagation();
+    evt.preventDefault();
+  
+    var files = evt.dataTransfer.files; // Lista de archivos arrastrados
+  
+    for (var i = 0, f; f = files[i]; i++) {
+      // Leer el archivo aquí. Por ejemplo, puedes enviarlo a un servidor
+      console.log(f);
+    }
+  }
+  
+  // Función para manejar los eventos de arrastrar sobre y arrastrar fuera
+  function handleDragOver(evt) {
+    evt.stopPropagation();
+    evt.preventDefault();
+    evt.dataTransfer.dropEffect = 'copy'; // Visualmente indica que se van a copiar los archivos
+  }
+  
+  // Obtener el elemento de la zona de arrastre
+  var dropZone = document.getElementById('drop_zone');
+  
+  // Agregar los listeners de los eventos
+  dropZone.addEventListener('dragover', handleDragOver, false);
+  dropZone.addEventListener('drop', handleFileSelect, false);
